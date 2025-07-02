ee4a3250fe7b0a1c7774c72835f6e085cd6819d9
PWM-Alex <VictorAlex1233@hotmail.com>
2025-07-02 11:15:52 -0600

    Validaciones en Register V.1
    Cambios en las validaciones de la pantalla de registro

--- src/Front-End-SIHAS/screens/auth/Register.js
+++ src/Front-End-SIHAS/screens/auth/Register.js
@@ -4,17 +4,62 @@ import Icon from 'react-native-vector-icons/Feather';
 
 export default function RegisterScreen() {
     const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [Lastname, setLastname] = useState("");
+    const [SecondLastname, setSecondLastname] = useState("");
     const [password, setPassword] = useState("");
     const [erroMessage, setErrorMessage] = useState(false);
+    const [erroPasswordMessage, setErroPasswordMessage] = useState(false);
     const [passwordVisible, setPasswordVisible] = useState(false);
 
-    const handleInputChange = (email) => {
+
+    const handleInputEmailChange = (email) => {
         if (!email) {
             setErrorMessage(true)
         } else {
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
             if (emailRegex.test(email) === true) {
                 setEmail(email)
+            } else {
+                setErrorMessage(true)
+            }
+        }
+    };
+
+
+    const handleInputNameChange = (name) => {
+        if (!name) {
+            setErrorMessage(true)
+        } else {
+            const textRegex = /^[a-zA-Z]+$/;
+            if (textRegex.test(name) === true) {
+                setName(name)
+            } else {
+                setErrorMessage(true)
+            }
+        }
+    };
+
+    const handleInputLastNameChange = (Lastname) => {
+        if (!Lastname) {
+            setErrorMessage(true)
+        } else {
+            const textRegex = /^[a-zA-Z]+$/;
+            if (textRegex.test(Lastname) === true) {
+                setLastname(Lastname)
+            } else {
+                setErrorMessage(true)
+            }
+        }
+    };
+
+    const handleInputSecondLastNameChange = (Lastname) => {
+        if (!Lastname) {
+            setErrorMessage(true)
+        } else {
+            const textRegex = /^[a-zA-Z]+$/;
+            if (textRegex.test(Lastname) === true) {
+                setSecondLastname(Lastname)
             } else {
                 setErrorMessage(true)
             }
@@ -35,44 +80,73 @@ export default function RegisterScreen() {
         }
     };
 
+
+    const verifyPassword = (trypassword) => {
+        if (trypassword != password) {
+            setErroPasswordMessage(true)
+        }
+    }
+
     const toggleVisibility = () => {
         setPasswordVisible(!passwordVisible)
     }
 
+    const handleRegister = async () => {
+        try {
+            if (email === '' && password === '' && name === '' && Lastname === '' && SecondLastname === '') {
+                setErrorMessage(true)
+                setErroPasswordMessage(true)
+            } else {
+                // Llamada a mi api
+            }
+        } catch {
+            setErrorMessage('');
+            setTimeout(() => setErrorMessage('Usuario o contraseña incorrectos'), 10);
+        }
+    }
+
+
+
     return (
         <SafeAreaView edges={['top']}>
-            <ScrollView style={{ backgroundColor: 'white', height: '100%' }}>
+            <ScrollView style={{ backgroundColor: 'white', height: '100%', paddingBottom: 10 }}>
+
                 <View style={styles.image}>
                     <Image style={styles.logo} source={require('../../assets/logo.jpg')} />
                 </View>
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Correo Electronico</Text>
                     <View style={styles.div}>
-                        <TextInput onChangeText={handleInputChange} style={styles.input} placeholder="Correo Electronico" required />
+                        <TextInput onChangeText={handleInputEmailChange} style={styles.input} placeholder="Correo Electronico" required />
                     </View>
                 </View>
                 {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un correo valido</Text>)}
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Nombre</Text>
                     <View style={styles.div}>
-                        <TextInput onChangeText={handleInputChange} style={styles.input} placeholder="Primer y segundo nombre" required />
+                        <TextInput onChangeText={handleInputNameChange} style={styles.input} placeholder="Primer y segundo nombre" required />
                     </View>
                 </View>
-                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un correo valido</Text>)}
+                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un nombre valido</Text>)}
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Apellido Paterno</Text>
                     <View style={styles.div}>
-                        <TextInput onChangeText={handleInputChange} style={styles.input} placeholder="Correo Electronico" required />
+                        <TextInput onChangeText={handleInputLastNameChange} style={styles.input} placeholder="Correo Electronico" required />
                     </View>
                 </View>
-                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un correo valido</Text>)}
+                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un apellido valido</Text>)}
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Apellido Materno</Text>
                     <View style={styles.div}>
-                        <TextInput onChangeText={handleInputChange} style={styles.input} placeholder="Correo Electronico" required />
+                        <TextInput onChangeText={handleInputSecondLastNameChange} style={styles.input} placeholder="Correo Electronico" required />
                     </View>
                 </View>
-                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un correo valido</Text>)}
+                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca un apellido valido</Text>)}
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Contraseña</Text>
                     <View style={styles.div}>
@@ -82,24 +156,25 @@ export default function RegisterScreen() {
                         </TouchableOpacity>
                     </View>
                 </View>
+
                 <View style={styles.container}>
                     <Text style={styles.inputext}>Confirmar Contraseña</Text>
                     <View style={styles.div}>
-                        <TextInput onChangeText={handlePasswordChange} style={styles.input} secureTextEntry={!passwordVisible} placeholder="Contraseña" required />
+                        <TextInput onChangeText={verifyPassword} style={styles.input} secureTextEntry={!passwordVisible} placeholder="Repite tu Contraseña" required />
                         <TouchableOpacity onPress={toggleVisibility} style={styles.icon}>
                             <Icon name={passwordVisible ? 'eye' : 'eye-off'} size={20} color="#666" />
                         </TouchableOpacity>
                     </View>
                 </View>
-                {erroMessage && (<Text style={styles.linkError}>Por Favor coloca una contraseña valida</Text>)}
+                {erroPasswordMessage && (<Text style={styles.linkError}>Por Favor coloca una contraseña valida</Text>)}
 
                 <View style={styles.divbutton}>
-                    <TouchableOpacity style={styles.button} >
+                    <TouchableOpacity style={styles.button} onPress={handleRegister}>
                         <Text style={styles.buttonText}>Registrarse</Text>
                     </TouchableOpacity>
                 </View>
 
-            </ScrollView>
+            </ScrollView >
         </SafeAreaView>
     )
 }
@@ -202,6 +277,7 @@ const styles = StyleSheet.create({
     },
     divbutton: {
         paddingTop: 25,
+        paddingBottom: 25,
         justifyContent: 'center',
         alignItems: 'center',
     },
