41c898943287a9107e31deb0cc92a8c4e95dd530
unknown <20223tn012@utez.edu.mx>
2025-07-02 08:31:23 -0600

    SS-45 clase y repositorio de alertas

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/alert/AlertRepository.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/alert/AlertRepository.java
@@ -1,4 +1,8 @@
 package utez.edu.mx.sihas.model.alert;
 
-public interface AlertRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlertRepository extends JpaRepository<Alert, Long> {
 }
