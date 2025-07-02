983abf34125bdac9f3af99d57064432940c0e6ba
chrisUtez175 <i20173ti175@utez.edu.mx>
2025-07-02 11:10:42 -0600

    Update BiologicalDataRepository.java

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/biological_data/BiologicalDataRepository.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/biological_data/BiologicalDataRepository.java
@@ -1,4 +1,7 @@
 package utez.edu.mx.sihas.model.biological_data;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BiologicalDataRepository  extends JpaRepository <BiologicalData,Long>{
 }
