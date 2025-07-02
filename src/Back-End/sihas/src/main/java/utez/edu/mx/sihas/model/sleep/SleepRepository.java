fb76083cbcac87abbac5075dfb34afb9e2764276
chrisUtez175 <i20173ti175@utez.edu.mx>
2025-07-02 11:11:10 -0600

    Update SleepRepository.java

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/sleep/SleepRepository.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/sleep/SleepRepository.java
@@ -1,4 +1,7 @@
 package utez.edu.mx.sihas.model.sleep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SleepRepository extends JpaRepository<Sleep,Long> {
 }
