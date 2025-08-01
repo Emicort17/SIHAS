dd012c8ddb2123861ba3a21bfb968ded7bffcc69
unknown <20223tn012@utez.edu.mx>
2025-08-01 17:45:51 -0600

    SS-89 [UPDATE] se corrigio la respuesta que tienes al momento de iniciar sesion

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/security/control/AuthController.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/security/control/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import utez.edu.mx.sihas.model.rol.Rol;
 import utez.edu.mx.sihas.model.user.User;
 import utez.edu.mx.sihas.model.user.UserRepository;
 import utez.edu.mx.sihas.security.JwtUtil;
@@ -51,6 +52,11 @@ public class AuthController {
 
         long expirationTime = jwtUtil.getExpirationTime();
 
-        return new AuthResponse(jwt, user.getId_user(), user.getEmail(), expirationTime);
+        String rolString =  user.getRoles().stream()
+                .findFirst()
+                .map(Rol::getName)
+                .orElse("Sin rol");
+
+        return new AuthResponse(jwt, user.getId_user(), user.getEmail(), rolString,expirationTime);
     }
 }
