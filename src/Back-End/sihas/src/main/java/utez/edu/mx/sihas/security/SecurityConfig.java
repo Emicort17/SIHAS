ab789a97b45203388c62ebeb7b5e6a353a439bd8
unknown <20223tn012@utez.edu.mx>
2025-08-01 18:42:47 -0600

    SS-89 [UPDATE] configuracion de cors en security

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/security/SecurityConfig.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/security/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
     private final JwtRequestFilter jwtRequestFilter;
@@ -32,8 +34,16 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
-
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .cors(cors -> cors.configurationSource(request -> {
+                    var corsConfig = new org.springframework.web.cors.CorsConfiguration();
+                    corsConfig.setAllowedOrigins(List.of("http://localhost"));
+                    corsConfig.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+                    corsConfig.setAllowedHeaders(List.of("*"));
+                    corsConfig.setAllowCredentials(true);
+                    corsConfig.addExposedHeader("Authorization");
+                    return corsConfig.applyPermitDefaultValues();
+                }));
         return http.build();
     }
 
