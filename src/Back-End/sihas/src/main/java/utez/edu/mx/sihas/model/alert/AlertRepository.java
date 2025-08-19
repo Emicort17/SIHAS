7ecf7ba21deeffb59d070ffe93606dfa6c4cfea1
unknown <20223tn012@utez.edu.mx>
2025-08-19 10:56:02 -0600

    [UPDATED] agrego un servicio para mandar las alertas por usuario

--- src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/alert/AlertRepository.java
+++ src/Back-End/sihas/src/main/java/utez/edu/mx/sihas/model/alert/AlertRepository.java
@@ -2,7 +2,11 @@ package utez.edu.mx.sihas.model.alert;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import utez.edu.mx.sihas.model.user.User;
+
+import java.util.List;
 
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, Long> {
+    List<Alert> findAllByUser(User user);
 }
